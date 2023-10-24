6e3233aa266f5903ec348af89579b1451f740a5c
vadimkosolapov <vilgutis@icloud.com>
2023-10-24 20:45:21 +0500

    1 commit

--- src/main/java/com/example/bulletinboard/repository/ImageRepo.java
+++ src/main/java/com/example/bulletinboard/repository/ImageRepo.java
@@ -3,5 +3,5 @@ package com.example.bulletinboard.repository;
 import com.example.bulletinboard.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepo extends JpaRepository<Image, String> {
+public interface ImageRepo extends JpaRepository<Image, Long> {
 }
