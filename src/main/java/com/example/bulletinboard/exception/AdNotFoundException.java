335369a81e476d7b7b3b12492f53f39ee24d7fbb
vadimkosolapov <vilgutis@icloud.com>
2023-11-02 01:04:16 +0500

    create JavaDoc in Exception

--- src/main/java/com/example/bulletinboard/exception/AdNotFoundException.java
+++ src/main/java/com/example/bulletinboard/exception/AdNotFoundException.java
@@ -1,6 +1,14 @@
 package com.example.bulletinboard.exception;
 
+
+/**
+ * Исключение <i>UserNotFoundException</i> является подклассом <i>NotFoundException</i>.
+ */
 public class AdNotFoundException extends NotFoundException{
+
+    /**
+     * Выбрасывает исключение с сообщением <i>"Объявление не найдено"</i>
+     */
     public AdNotFoundException() {
         super("Объявление не найдено");
     }
